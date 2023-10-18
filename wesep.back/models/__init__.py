502f17ced93c15f2082e532016c3189e1273ce74
epicmouse667 <53855932+epicmouse667@users.noreply.github.com>
2023-10-18 15:58:24 +0800

    Delete wesep.back directory

--- wesep.back/models/__init__.py
+++
@@ -1,9 +0,0 @@
-import wesep.models.convtasnet as convtasnet
-
-
-def get_model(model_name: str):
-    if model_name.startswith("ConvTasNet"):
-        return getattr(convtasnet, model_name)
-    else:  # model_name error !!!
-        print(model_name + " not found !!!")
-        exit(1)
--- wesep.back/modules/tasnet/__init__.py
+++
@@ -1,3 +0,0 @@
-from wesep.modules.tasnet.decoder import DeepDecoder
-from wesep.modules.tasnet.encoder import DeepEncoder
-from wesep.modules.tasnet.separation import Separation
