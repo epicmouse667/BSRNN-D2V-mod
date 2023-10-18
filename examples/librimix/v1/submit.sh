e05381eb0aa48a7be5a03eeaba57247bccb3b8fb
epicmouse667 <53855932+epicmouse667@users.noreply.github.com>
2023-10-18 15:58:52 +0800

    Delete examples/librimix/v1 directory

--- examples/librimix/v1/submit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=0,2,4 ./run.sh --stage 3 --stop-stage 3 \
---config exp/BSRNN_D2V/train100/pretrained_extraction/config.yaml \
---exp_dir exp/BSRNN_D2V/train100/pretrained_extraction/ \
---gpus '[0,1,2]'
